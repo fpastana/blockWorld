4acc2317fcef7b6448e5ebcba3c92cd0815849f2
fpastana <felipeapastana@gmail.com>
2023-03-06 13:40:07 -0700

    It includes the possibility to run the the app through the terminal passing a txt file with the inputs as a PHP argument

---
+++ BlockWorld.php
@@ -0,0 +1,234 @@
+<?php
+
+class BlockWorld 
+{
+    private array $blocks = [];
+
+    public function __construct(int $n = 0)
+    {
+        for ($i=0; $i<=$n-1; $i++) {
+            $this->blocks[$i]['stack'][$i] = $i;
+        }
+    }
+
+    public function moveOnto(int $a, int $b): void
+    {
+        if ($this->checkInconsistency($a, $b) === true) 
+            return;
+
+        if ($this->checkIfSameStack($a, $b)) 
+            return;
+
+        $blockDestination = $this->retrieveBlockDestination($a, $b);
+
+        foreach ($this->blocks as $ind => $block) {   
+            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['a' => $a]);
+            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['b' => $b]);
+        }
+
+        unset($this->blocks[$a]['stack'][$a]);
+        $this->blocks[$blockDestination]['stack'][$a] = $a;
+    }
+
+    public function moveOver(int $a, int $b): void
+    {
+        if ($this->checkInconsistency($a, $b) === true) 
+            return;
+
+        if ($this->checkIfSameStack($a, $b)) 
+        return;
+
+        $blockDestination = $this->retrieveBlockDestination($a, $b);
+
+        foreach ($this->blocks as $ind => $block) {   
+            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['a' => $a]);
+        }
+
+        unset($this->blocks[$a]['stack'][$a]);
+        $this->blocks[$blockDestination]['stack'][$a] = $a;
+    }
+
+    public function pileOnto(int $a, int $b): void
+    {
+        if ($this->checkInconsistency($a, $b) === true) 
+            return;
+
+        $blockDestination = $this->retrieveBlockDestination($a, $b);
+
+        foreach ($this->blocks as $ind => $block) {
+            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['b' => $b]); 
+        }
+
+        foreach ($this->blocks as $ind => $block) {
+            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndRelocateNewStack'], ['a' => $a, 'b' => $b]);
+        }
+
+        unset($this->blocks[$a]['stack'][$a]);
+        $this->blocks[$blockDestination]['stack'][$a] = $a;
+    }
+
+    public function pileOver(int $a, int $b): void
+    {
+        if ($this->checkInconsistency($a, $b) === true) 
+            return;
+
+        $blockDestination = $this->retrieveBlockDestination($a, $b);
+
+        foreach ($this->blocks as $ind => $block) {
+            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndRelocateNewStack'], ['a' => $a, 'b' => $b]);
+        }
+
+        unset($this->blocks[$a]['stack'][$a]);
+        $this->blocks[$blockDestination]['stack'][$a] = $a;
+    }
+
+    public function quit(): string
+    {
+        $result = '';
+
+        foreach( $this->blocks as $ind => $blocks )
+        {
+            foreach ($this->blocks[$ind] as $stacks) {
+                $result .= $ind . ':';
+
+                foreach ($stacks as $stack) {
+                    $result .= ' ' . $stack;
+                }
+            }
+
+            $result .= "\r\n";
+        }
+
+        return $result;
+    }
+
+    public function readTxtFile(string $txtAddress, bool $printInput = false): ?string
+    {
+        $i = 0;
+
+        $fh = fopen($txtAddress,'r');
+        while ($command = fgets($fh)) {
+            
+            if ($i === 0) {
+                $blockWorld = new BlockWorld($command);
+            } else {
+                $commandArray = explode(' ', $command);
+
+                $first = trim($commandArray[0]);
+                if (isset($commandArray[1])) {
+                    $a = trim($commandArray[1]);
+                    $second = trim($commandArray[2]);
+                    $b = trim($commandArray[3]);
+
+                    if ($first === 'move' && $second === 'onto') {
+                        $blockWorld->moveOnto($a, $b);
+                    }
+
+                    if ($first === 'move' && $second === 'over') {
+                        $blockWorld->moveOver($a, $b);
+                    }
+
+                    if ($first === 'pile' && $second === 'onto') {
+                        $blockWorld->pileOnto($a, $b);
+                    }
+
+                    if ($first === 'pile' && $second === 'over') {
+                        $blockWorld->pileOver($a, $b);
+                    }
+                }
+
+                if ($first === 'quit') {
+                    return $blockWorld->quit();
+                }
+            }
+
+            $i++;
+        }
+        fclose($fh);
+
+        return null;
+    }
+
+    public function printInput(string $txtAddress): string
+    {
+        $text = '';
+        $fh = fopen($txtAddress,'r');
+        while ($line = fgets($fh)) {
+            $text .= $line;
+        }
+        fclose($fh);
+
+        return $text;
+    }
+
+    public function getBlocks(): array
+    {
+        return $this->blocks;
+    }
+
+    private function unsetAndReturnStackToInitialPositions(array &$items, string $key, array $params, bool &$active = false): void
+    {
+        foreach ($items as $ind => $item) {        
+            if ($item === current($params)) {        
+                unset($items[$ind]);
+                $active = true;
+            }
+    
+            if ($active === true) {
+                unset($items[$ind]);
+                $this->blocks[$item]['stack'][$item] = $item;
+            }
+        }
+    }
+
+    private function unsetAndRelocateNewStack(array &$items, string $key, array $params, bool &$active = false): void
+    {
+        foreach ($items as $ind => $item) {        
+            if ($item === $params['a']) {        
+                unset($items[$ind]);
+                $active = true;
+            }
+    
+            if ($active === true) {
+                unset($items[$ind]);
+                $this->blocks[$params['b']]['stack'][$item] = $item;
+            }
+        }
+    }
+
+    private function checkInconsistency(int $a, int $b): bool
+    {
+        if ($a === $b) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function checkIfSameStack(int $a, int $b): bool 
+    {
+        $checkSameStack = [];
+
+        foreach ($this->blocks as $blockId => $block) {
+            $checkSameStack[$blockId] = array_key_exists($a, $block['stack']) && array_key_exists($b, $block['stack']);
+        }
+
+        return in_array(true, $checkSameStack) === true;
+    }
+
+    private function retrieveBlockDestination(int $a, int $b): int
+    {
+        if ($this->checkIfSameStack($a, $b) === true) {
+            return $b;
+        }
+
+        foreach ($this->blocks as $blockId => $block) {
+            $search[$blockId] = array_search($b, $block['stack']);
+        }
+
+        $removeBlank = array_filter($search);
+        $blockDestination = key($removeBlank);
+
+        return $blockDestination;
+    }
+}
--- index.php
+++ index.php
@@ -1,189 +1,25 @@
 <?php
 
-class BlockWorld 
-{
-    private array $blocks = [];
+require 'BlockWorld.php';
 
-    public function __construct(int $n)
-    {
-        for ($i=0; $i<=$n-1; $i++) {
-            $this->blocks[$i]['stack'][$i] = $i;
-        }
-    }
-
-    public function moveOnto(int $a, int $b): void
-    {
-        if ($this->checkInconsistency($a, $b) === true) 
-            return;
-
-        if ($this->checkIfSameStack($a, $b)) 
-            return;
-
-        $blockDestination = $this->retrieveBlockDestination($a, $b);
-
-        foreach ($this->blocks as $ind => $block) {   
-            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['a' => $a]);
-            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['b' => $b]);
-        }
-
-        unset($this->blocks[$a]['stack'][$a]);
-        $this->blocks[$blockDestination]['stack'][$a] = $a;
-    }
-
-    public function moveOver(int $a, int $b): void
-    {
-        if ($this->checkInconsistency($a, $b) === true) 
-            return;
-
-        if ($this->checkIfSameStack($a, $b)) 
-        return;
-
-        $blockDestination = $this->retrieveBlockDestination($a, $b);
-
-        foreach ($this->blocks as $ind => $block) {   
-            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['a' => $a]);
-        }
-
-        unset($this->blocks[$a]['stack'][$a]);
-        $this->blocks[$blockDestination]['stack'][$a] = $a;
-    }
-
-    public function pileOnto(int $a, int $b): void
-    {
-        if ($this->checkInconsistency($a, $b) === true) 
-            return;
-
-        $blockDestination = $this->retrieveBlockDestination($a, $b);
-
-        foreach ($this->blocks as $ind => $block) {
-            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndReturnStackToInitialPositions'], ['b' => $b]); 
-        }
-
-        foreach ($this->blocks as $ind => $block) {
-            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndRelocateNewStack'], ['a' => $a, 'b' => $b]);
-        }
-
-        unset($this->blocks[$a]['stack'][$a]);
-        $this->blocks[$blockDestination]['stack'][$a] = $a;
-    }
-
-    public function pileOver(int $a, int $b): void
-    {
-        if ($this->checkInconsistency($a, $b) === true) 
-            return;
-
-        $blockDestination = $this->retrieveBlockDestination($a, $b);
-
-        foreach ($this->blocks as $ind => $block) {
-            array_walk($this->blocks[$ind], ['BlockWorld', 'unsetAndRelocateNewStack'], ['a' => $a, 'b' => $b]);
-        }
-
-        unset($this->blocks[$a]['stack'][$a]);
-        $this->blocks[$blockDestination]['stack'][$a] = $a;
-    }
-
-    public function quit(): void
-    {
-        $results = [];
-
-        echo '</br></br>';
-
-        foreach( $this->blocks as $ind => $blocks )
-        {
-            foreach ($this->blocks[$ind] as $stacks) {
-                echo $ind . ':';
-
-                foreach ($stacks as $stack) {
-                    echo ' ' . $stack;
-                }
-            }
-
-            echo '<br/>';
-        }
-    }
-
-    public function getBlocks(): array
-    {
-        return $this->blocks;
-    }
-
-    private function unsetAndReturnStackToInitialPositions(array &$items, string $key, array $params, bool &$active = false): void
-    {
-        foreach ($items as $ind => $item) {        
-            if ($item === current($params)) {        
-                unset($items[$ind]);
-                $active = true;
-            }
-    
-            if ($active === true) {
-                unset($items[$ind]);
-                $this->blocks[$item]['stack'][$item] = $item;
-            }
-        }
-    }
-
-    private function unsetAndRelocateNewStack(array &$items, string $key, array $params, bool &$active = false): void
-    {
-        foreach ($items as $ind => $item) {        
-            if ($item === $params['a']) {        
-                unset($items[$ind]);
-                $active = true;
-            }
-    
-            if ($active === true) {
-                unset($items[$ind]);
-                $this->blocks[$params['b']]['stack'][$item] = $item;
-            }
-        }
-    }
-
-    private function checkInconsistency(int $a, int $b): bool
-    {
-        if ($a === $b) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function checkIfSameStack(int $a, int $b): bool 
-    {
-        $checkSameStack = [];
-
-        foreach ($this->blocks as $blockId => $block) {
-            $checkSameStack[$blockId] = array_key_exists($a, $block['stack']) && array_key_exists($b, $block['stack']);
-        }
-
-        return in_array(true, $checkSameStack) === true;
-    }
-
-    private function retrieveBlockDestination(int $a, int $b): int
-    {
-        if ($this->checkIfSameStack($a, $b) === true) {
-            return $b;
-        }
-
-        foreach ($this->blocks as $blockId => $block) {
-            $search[$blockId] = array_search($b, $block['stack']);
-        }
-
-        $removeBlank = array_filter($search);
-        $blockDestination = key($removeBlank);
-
-        return $blockDestination;
-    }
+if (!isset($argv[1])) {
+    echo "\r\n" . 'Warning: It is necessary to provide a .txt file with the correct input!' . "\r\n\r\n";
+    exit;
 }
 
-$blockWorld = new BlockWorld(10);
-$blockWorld->moveOnto(9, 1);
-$blockWorld->moveOver(8, 1);
-$blockWorld->moveOver(7, 1);
-$blockWorld->moveOver(6, 1);
-$blockWorld->pileOver(8, 6);
-$blockWorld->pileOver(8, 5);
-$blockWorld->moveOver(2, 1);
-$blockWorld->moveOver(4, 9);
+// $blockWorld = new BlockWorld(10);
+// $blockWorld->moveOnto(9, 1);
+// $blockWorld->moveOver(8, 1);
+// $blockWorld->moveOver(7, 1);
+// $blockWorld->moveOver(6, 1);
+// $blockWorld->pileOver(8, 6);
+// $blockWorld->pileOver(8, 5);
+// $blockWorld->moveOver(2, 1);
+// $blockWorld->moveOver(4, 9);
+// echo $blockWorld->quit();
 
-echo '<pre>';
-print_r($blockWorld->getBlocks());
-$blockWorld->quit();
+$blockWorld = new BlockWorld();
+echo 'Input: ' . "\r\n";
+echo $blockWorld->printInput($argv[1], true);
+echo "\r\n\r\n" . 'Output: ' . "\r\n";
+echo $blockWorld->readTxtFile($argv[1], true);
